17e510f25a41bef8cda8e8c2db257c8f1bb9dde3
gabrielSantos1101 <gabriel.adv45@outlook.com>
2024-06-05 22:42:14 -0300

    feat: Implement cookie retrieval in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,23 @@
-// This is the service worker script, which executes in its own context
-// when the extension is installed or refreshed (or when you access its console).
-// It would correspond to the background script in chrome extensions v2.
+importScripts("service-worker-utils.js");
 
-console.log("This prints to the console of the service worker (background script)")
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "getCookieValues") {
+    const cookieNames = request.cookieNames;
+    const cookieValues = {};
 
-// Importing and using functionality from external files is also possible.
-importScripts('service-worker-utils.js')
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTab = tabs[0];
+      chrome.cookies.getAll({ url: activeTab.url }, (cookies) => {
+        for (const cookie of cookies) {
+          if (cookieNames.includes(cookie.name)) {
+            cookieValues[cookie.name] = cookie.value;
+          }
+        }
+        cookieValues.platform = request.platform;
+        sendResponse(cookieValues);
+      });
+    });
 
-// If you want to import a file that is deeper in the file hierarchy of your
-// extension, simply do `importScripts('path/to/file.js')`.
-// The path should be relative to the file `manifest.json`.
+    return true;
+  }
+});
